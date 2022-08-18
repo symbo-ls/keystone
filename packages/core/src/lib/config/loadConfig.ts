9c90b2bef575461abe8722f94d65c781091d4d83
mitchellhamilton <mitchell@hamil.town>
2022-08-18 14:00:02 +1000

    use esbuild

--- packages/core/src/lib/config/loadConfig.ts
+++ packages/core/src/lib/config/loadConfig.ts
@@ -1,8 +1,33 @@
-import { getConfigPath } from '../../scripts/utils';
+import esbuild, { BuildOptions } from 'esbuild';
 import { KeystoneConfig } from '../../types';
+import { getBuiltConfigPath } from '../../scripts/utils';
 import { initConfig } from './initConfig';
-import { requireSource } from './requireSource';
 
-export async function loadConfig(cwd: string): Promise<KeystoneConfig> {
-  return initConfig(requireSource(getConfigPath(cwd)).default);
+export function getEsbuildConfig(cwd: string): BuildOptions {
+  return {
+    entryPoints: ['./keystone'],
+    absWorkingDir: cwd,
+    bundle: true,
+    outfile: '.keystone/config.js',
+    format: 'cjs',
+    plugins: [
+      {
+        name: 'external-node_modules',
+        setup(build) {
+          build.onResolve({ filter: /(?:^[^.][^/])|(?:^[^.][^.][^/])|(?:^.{0,2}$)/ }, args => {
+            return { external: true, path: args.path };
+          });
+        },
+      },
+    ],
+  };
+}
+
+export function loadBuiltConfig(cwd: string): KeystoneConfig {
+  return initConfig(require(getBuiltConfigPath(cwd)).default);
+}
+
+export async function loadConfigOnce(cwd: string): Promise<KeystoneConfig> {
+  await esbuild.build(getEsbuildConfig(cwd));
+  return loadBuiltConfig(cwd);
 }
